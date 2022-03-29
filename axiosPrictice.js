960596621b2526524fb334745bd2440028914907
peter.chen1024 <u0052017@gmail.com>
2022-03-29 18:16:52 +0800

    add axios prictice

---
+++ axiosPrictice.js
@@ -0,0 +1,19 @@
+
+// 前端薪資 API
+const api = 'https://raw.githubusercontent.com/hexschool/2021-ui-frontend-job/master/frontend_data.json'
+
+const axios = {
+  get: function(url) {
+    return new Promise((resolve, reject)=>{
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", url);
+      xhr.onload = () => resolve(xhr.responseText);
+      xhr.onerror = () => reject(xhr.statusText);
+      xhr.send();
+    })
+  }
+}
+
+axios.get(api)
+  .then(data=> console.log(JSON.parse(data)))
+  .catch(err=> console.log(err))
